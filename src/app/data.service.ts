4c12bcd1404171cb7b32e89b809e11301b836e12
asmi-kafle <kafleasmi1@gmail.com>
2020-07-08 04:37:28 -0500

    Add comments in data.service.ts for HTTP methods

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -14,11 +14,14 @@ export class DataService {
   constructor(private http: HttpClient) {}
 
   // HTTP Get Request
-
+  // sends a request and received an observable of HTTPResponse from the backend API.
+  // the HTTPResponse has a header and a body.
+  // The header stores the error code and message and the body stores the data gotten back from the get request
   getData(): Observable<HttpResponse<Data[]>> {
     return this.http.get<Data[]>(localUrl, { observe: 'response' });
   }
 
+  //subscribes to the response's body and loops until the new row of data is received and returns those data
   getNotesData(): Data[] {
     this.getData().subscribe((resp) => {
       console.log(resp);
@@ -32,7 +35,7 @@ export class DataService {
   }
 
   // HTTP Delete Request
-
+  //sends the uri pointing to a specific entity and removes that row from the database
   deleteData(id: number): Observable<HttpResponse<Data[]>> {
     console.log('DeleteData was called.');
     return this.http.delete<Data[]>(localUrl + '/' + id, {
@@ -48,7 +51,7 @@ export class DataService {
   }
 
   // HTTP Put Request
-
+  //sends uri pointing to a specific entity in the database and updates it with the information sent
   putData(data: Data) {
     console.log('putData from service was called');
     var response = this.http.put(localUrl + '/' + data.id, data);
@@ -61,7 +64,7 @@ export class DataService {
   }
 
   // HTTP Post Request
-
+  //sends the data, of the type defined in the Data interface, to be added in the database in a new row
   postData(data: Data) {
     console.log('Post data was called inside DataService');
     var response = this.http.post(localUrl, data);
